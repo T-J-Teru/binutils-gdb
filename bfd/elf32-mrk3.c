ae8c2974536a6c0873d94f4114842edb8312e7ff
Simon Cook <simon.cook@embecosm.com>
2014-06-13 18:23:48 +0100

    Add R_MRK3_WORD16 relocation.
    This relocation is the same as R_MRK3_16, but shifts values to point to
    words instead of bytes. It is to be used for example with a jump table.
    
    bfd:
        * elf32-mrk3.c (elf_mrk3_howto_table): Add R_MRK3_WORD16 relocation.
    
    include:
        * elf/mrk3.h: Add R_MRK3_WORD16 relocation.

--- bfd/elf32-mrk3.c
+++ bfd/elf32-mrk3.c
@@ -111,6 +111,19 @@ static reloc_howto_type elf_mrk3_howto_table[] =
    0xffff0000,            /* Src_mask.  */
    0xffff0000,            /* Dst_mask.  */
    FALSE),                /* PCrel_offset.  */
+  HOWTO (R_MRK3_WORD16,             /* Type.  */
+   1,                     /* Rightshift.  */
+   1,                     /* Size (0 = byte, 1 = short, 2 = long).  */
+   16,                    /* Bitsize.  */
+   FALSE,                 /* PC_relative.  */
+   0,                     /* Bitpos.  */
+   complain_overflow_bitfield, /* Complain_on_overflow.  */
+   bfd_elf_generic_reloc, /* Special_function.  */
+   "R_MRK3_WORD16",       /* Name.  */
+   TRUE,                  /* Partial_inplace.  */
+   0x1ffff,                /* Src_mask.  */
+   0xffff,                /* Dst_mask.  */
+   FALSE),                /* PCrel_offset.  */
 };
 
 /* Map BFD reloc types to MRK3 ELF reloc types.  */
