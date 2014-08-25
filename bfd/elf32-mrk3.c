702f8c92bcd3055b32218916348bb6eab265ddbd
Simon Cook <simon.cook@embecosm.com>
2014-08-25 13:50:04 +0100

    Add handling of R_MRK3_AUTO16 relocation.
    This relocation is generated by LLVM as it is unable to determine in the
    case of jump tables/constant tables whether a symbol being referenced is
    in a code section or data section and therefore whether it should be
    a word or data pointer. In this case, the linker decides based on the
    section (if it has the code flag set) and shifts accordingly.
    
    bfd:
    	* elf32-mrk3.c (elf_mrk3_howto_table): Add R_MRK3_AUTO16 relocation.
    	(mrk3_final_link_relocate): Add function to automatically determine
    	whether to bitshift depending on whether symbol is in code space.
    	(mrk3_elf_relocate_section): Added to utilize
    	mrk3_final_link_relocate.
    
    include:
    	* elf/mrk3.h: Add R_MRK3_AUTO16 relocation.

--- bfd/elf32-mrk3.c
+++ bfd/elf32-mrk3.c
@@ -124,6 +124,19 @@ static reloc_howto_type elf_mrk3_howto_table[] =
    0x1ffff,                /* Src_mask.  */
    0xffff,                /* Dst_mask.  */
    FALSE),                /* PCrel_offset.  */
+  HOWTO (R_MRK3_AUTO16,             /* Type.  */
+	 0,                     /* Rightshift.  */
+	 1,                     /* Size (0 = byte, 1 = short, 2 = long).  */
+	 16,                    /* Bitsize.  */
+	 FALSE,                 /* PC_relative.  */
+	 0,                     /* Bitpos.  */
+	 complain_overflow_bitfield, /* Complain_on_overflow.  */
+	 bfd_elf_generic_reloc, /* Special_function.  */
+	 "R_MRK3_AUTO16",       /* Name.  */
+	 TRUE,                  /* Partial_inplace.  */
+	 0x1ffff,                /* Src_mask.  */
+	 0xffff,                /* Dst_mask.  */
+	 FALSE),                /* PCrel_offset.  */
 };
 
 /* Map BFD reloc types to MRK3 ELF reloc types.  */
@@ -194,6 +207,192 @@ mrk3_elf_object_p (bfd *abfd)
   return bfd_default_set_arch_mach (abfd, bfd_arch_mrk3, mach);
 }
 
+/* Perform a single relocation.
+   By default we use the standard BFD routines.  */
+
+static bfd_reloc_status_type
+mrk3_final_link_relocate (reloc_howto_type *  howto,
+			  bfd *               input_bfd,
+			  asection *          input_section,
+			  bfd_byte *          contents,
+			  Elf_Internal_Rela * rel,
+			  bfd_vma             relocation,
+			  asection *          symbol_section)
+{
+  switch (howto->type)
+    {
+      /* In the case of an automatic code/data relocation for MRK3, the
+	 linker decides whether to apply a bitshift or not. This is done
+	 based on whether the symbol being relocated against is stored in
+	 a section which has the code bit set.  */
+    case R_MRK3_AUTO16:
+      if (symbol_section && symbol_section->flags & SEC_CODE)
+	{
+	  relocation >>= 1;
+	}
+      break;
+      /* Pass others through.  */
+    default:
+      break;
+    }
+
+  /* Only install relocation if above tests did not disqualify it.  */
+  return _bfd_final_link_relocate (howto, input_bfd, input_section,
+				   contents, rel->r_offset,
+				   relocation, rel->r_addend);
+}
+
+/* Relocate an MRK3 ELF section.
+
+   The RELOCATE_SECTION function is called by the new ELF backend linker
+   to handle the relocations for a section.
+
+   The relocs are always passed as Rela structures; if the section
+   actually uses Rel structures, the r_addend field will always be
+   zero.
+
+   This function is responsible for adjusting the section contents as
+   necessary, and (if using Rela relocs and generating a relocatable
+   output file) adjusting the reloc addend as necessary.
+
+   This function does not have to worry about setting the reloc
+   address or the reloc symbol index.
+
+   LOCAL_SYMS is a pointer to the swapped in local symbols.
+
+   LOCAL_SECTIONS is an array giving the section in the input file
+   corresponding to the st_shndx field of each local symbol.
+
+   The global hash table entry for the global symbols can be found
+   via elf_sym_hashes (input_bfd).
+
+   When generating relocatable output, this function must handle
+   STB_LOCAL/STT_SECTION symbols specially.  The output symbol is
+   going to be the section symbol corresponding to the output
+   section, which means that the addend must be adjusted
+   accordingly.  */
+
+static bfd_boolean
+mrk3_elf_relocate_section (bfd *output_bfd ATTRIBUTE_UNUSED,
+			       struct bfd_link_info *info,
+			       bfd *input_bfd,
+			       asection *input_section,
+			       bfd_byte *contents,
+			       Elf_Internal_Rela *relocs,
+			       Elf_Internal_Sym *local_syms,
+			       asection **local_sections)
+{
+  Elf_Internal_Shdr *symtab_hdr;
+  struct elf_link_hash_entry **sym_hashes;
+  Elf_Internal_Rela *rel;
+  Elf_Internal_Rela *relend;
+
+  symtab_hdr = & elf_tdata (input_bfd)->symtab_hdr;
+  sym_hashes = elf_sym_hashes (input_bfd);
+  relend     = relocs + input_section->reloc_count;
+
+  for (rel = relocs; rel < relend; rel ++)
+    {
+      reloc_howto_type *           howto;
+      unsigned long                r_symndx;
+      Elf_Internal_Sym *           sym;
+      asection *                   sec;
+      struct elf_link_hash_entry * h;
+      bfd_vma                      relocation;
+      bfd_reloc_status_type        r;
+      const char *                 name = NULL;
+      int                          r_type ATTRIBUTE_UNUSED;
+
+      r_type = ELF32_R_TYPE (rel->r_info);
+      r_symndx = ELF32_R_SYM (rel->r_info);
+      howto  = elf_mrk3_howto_table + ELF32_R_TYPE (rel->r_info);
+      h      = NULL;
+      sym    = NULL;
+      sec    = NULL;
+
+      if (r_symndx < symtab_hdr->sh_info)
+	{
+	  sym = local_syms + r_symndx;
+	  sec = local_sections [r_symndx];
+	  relocation = BASEADDR (sec) + sym->st_value;
+
+	  name = bfd_elf_string_from_elf_section
+	    (input_bfd, symtab_hdr->sh_link, sym->st_name);
+	  name = (name == NULL) ? bfd_section_name (input_bfd, sec) : name;
+	}
+      else
+	{
+	  bfd_boolean warned ATTRIBUTE_UNUSED;
+	  bfd_boolean unresolved_reloc ATTRIBUTE_UNUSED;
+	  bfd_boolean ignored ATTRIBUTE_UNUSED;
+
+	  RELOC_FOR_GLOBAL_SYMBOL (info, input_bfd, input_section, rel,
+				   r_symndx, symtab_hdr, sym_hashes,
+				   h, sec, relocation,
+				   unresolved_reloc, warned, ignored);
+
+	  name = h->root.root.string;
+	}
+
+      if (sec != NULL && discarded_section (sec))
+	RELOC_AGAINST_DISCARDED_SECTION (info, input_bfd, input_section,
+					 rel, 1, relend, howto, 0, contents);
+
+      if (info->relocatable)
+	continue;
+
+      /* Finally, the sole MRK3-specific part.  */
+      r = mrk3_final_link_relocate (howto, input_bfd, input_section,
+				     contents, rel, relocation, sec);
+
+      if (r != bfd_reloc_ok)
+	{
+	  const char * msg = NULL;
+
+	  switch (r)
+	    {
+	    case bfd_reloc_overflow:
+	      r = info->callbacks->reloc_overflow
+		(info, (h ? &h->root : NULL), name, howto->name,
+		 (bfd_vma) 0, input_bfd, input_section, rel->r_offset);
+	      break;
+
+	    case bfd_reloc_undefined:
+	      r = info->callbacks->undefined_symbol
+		(info, name, input_bfd, input_section, rel->r_offset, TRUE);
+	      break;
+
+	    case bfd_reloc_outofrange:
+	      msg = _("internal error: out of range error");
+	      break;
+
+	      /* This is how mrk3_final_link_relocate tells us of a
+		 non-kosher reference between insn & data address spaces.  */
+	    case bfd_reloc_notsupported:
+	      if (sym != NULL) /* Only if it's not an unresolved symbol.  */
+		 msg = _("unsupported relocation between data/insn address spaces");
+	      break;
+
+	    case bfd_reloc_dangerous:
+	      msg = _("internal error: dangerous relocation");
+	      break;
+
+	    default:
+	      msg = _("internal error: unknown error");
+	      break;
+	    }
+
+	  if (msg)
+	    r = info->callbacks->warning
+	      (info, msg, name, input_bfd, input_section, rel->r_offset);
+
+	  if (! r)
+	    return FALSE;
+	}
+    }
+
+  return TRUE;
+}
 
 #define TARGET_LITTLE_SYM   bfd_elf32_mrk3_vec
 #define TARGET_LITTLE_NAME  "elf32-mrk3"
@@ -206,5 +405,6 @@ mrk3_elf_object_p (bfd *abfd)
 #define elf_info_to_howto                   0
 #define elf_info_to_howto_rel               mrk3_info_to_howto_rel
 #define elf_backend_object_p                mrk3_elf_object_p
+#define elf_backend_relocate_section		mrk3_elf_relocate_section
 
 #include "elf32-target.h"
