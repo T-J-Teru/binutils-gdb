3166656616fd5d49fd1afb4ec4e50200bf8e3ad0
Simon Cook <simon.cook@embecosm.com>
2015-01-13 12:56:50 +0000

    MRK3/bfd: Fill holes in files with NOPs instead of 0s
    bfd:
    
    	* cpu-mrk3.c (bfd_arch_mrk3_fill): Added.
    	(bfd_mrk3_arch): Use bfd_arch_mrk3_fill to fill holes with
    	NOPs.

--- bfd/cpu-mrk3.c
+++ bfd/cpu-mrk3.c
@@ -24,6 +24,31 @@
 #include "bfd.h"
 #include "libbfd.h"
 
+/* Fill buffer with NOPs */
+static void *
+bfd_arch_mrk3_fill (bfd_size_type count,
+		    bfd_boolean is_bigendian ATTRIBUTE_UNUSED,
+		    bfd_boolean code ATTRIBUTE_UNUSED)
+{
+  /* mov.b r0l, r0l */
+  static const char nop[] = { 0x40, 0x68 };
+  bfd_size_type nop_size = 2;
+
+  void *fill = bfd_malloc (count);
+  if (fill != NULL)
+    {
+      bfd_byte *p = fill;
+      while (count >= nop_size) {
+	memcpy (p, nop, nop_size);
+	p += nop_size;
+	count -= nop_size;
+      }
+      if (count != 0)
+	memset (p, 0, count);
+    }
+  return fill;
+}
+
 
 const bfd_arch_info_type bfd_mrk3_arch =
 {
@@ -38,6 +63,6 @@ const bfd_arch_info_type bfd_mrk3_arch =
   TRUE,                   /* the default ? */
   bfd_default_compatible, /* architecture comparison fn */
   bfd_default_scan,       /* string to architecture convert fn */
-  bfd_arch_default_fill,  /* Default fill.  */
-  NULL				            /* next in list */
+  bfd_arch_mrk3_fill,     /* MRK3 fill.  */
+  NULL			  /* next in list */
 };
