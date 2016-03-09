cad4e9d011b7bacc4b701f1fc977e5fca8a782dd
Andrew Burgess <andrew.burgess@embecosm.com>
2016-03-09 22:08:34 +0000

    patches/02820_37328-frame-this-id.patch

--- gdb/dwarf2-frame.c
+++ gdb/dwarf2-frame.c
@@ -1274,12 +1274,6 @@ dwarf2_frame_this_id (struct frame_info *this_frame, void **this_cache,
   struct dwarf2_frame_cache *cache =
     dwarf2_frame_cache (this_frame, this_cache);
 
-  if (cache->unavailable_retaddr)
-    return;
-
-  if (cache->undefined_retaddr)
-    return;
-
   (*this_id) = frame_id_build (cache->cfa, get_frame_func (this_frame));
 }
 
