487e3e5adbd13ccf4d9404eaa840e2e7281c9a28
Andrew Burgess <andrew.burgess@embecosm.com>
2016-03-09 22:08:37 +0000

    patches/03210-ALL-513-add-test-for-info-sources-by-binary

---
+++ gdb/testsuite/gdb.base/info-sources-by-binary-shlib.c
@@ -0,0 +1,6 @@
+#include "info-sources-by-binary-shlib.h"
+
+float testFunction(int number)
+{
+    return (float) number * 1.5f;
+}
---
+++ gdb/testsuite/gdb.base/info-sources-by-binary-target.c
@@ -0,0 +1,9 @@
+#include "info-sources-by-binary-shlib.h"
+
+int main()
+{
+    int number = 5;
+    float result = testFunction(number);
+    return 0;
+}
+
