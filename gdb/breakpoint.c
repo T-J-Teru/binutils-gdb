39446ef429cc8694d27b2137ddaedbff3ea0c861
Andrew Burgess <andrew.burgess@embecosm.com>
2016-03-09 22:08:27 +0000

    patches/01885_inf-num-not-upc-thread.patch

--- gdb/breakpoint.c
+++ gdb/breakpoint.c
@@ -6242,7 +6242,10 @@ output_thread_groups (struct ui_out *uiout,
 	  else
 	    ui_out_text (uiout, ", ");
 
-	  ui_out_text (uiout, plongest (upc_thread_of_inferior(inf)));
+	  if (upcmode)
+	    ui_out_text (uiout, plongest (upc_thread_of_inferior(inf)));
+	  else
+	    ui_out_text (uiout, plongest (inf->num));
 	}
     }
 
