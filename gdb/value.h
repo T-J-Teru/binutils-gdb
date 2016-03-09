a0a6696aee97c5c41bc198f98dc30ce6aa3003a3
Andrew Burgess <andrew.burgess@embecosm.com>
2016-03-09 22:08:32 +0000

    patches/02670_32148-watchpoint.patch

--- gdb/value.h
+++ gdb/value.h
@@ -1117,4 +1117,6 @@ extern unsigned value_length (const struct value *value);
 extern void value_copy_contents (struct value *to, struct value *from);
 extern void value_copy_contents_all_raw (struct value *to, struct value *from);
 
+extern void value_contents_ensure_unlimited(struct value* val);
+
 #endif /* !defined (VALUE_H) */
