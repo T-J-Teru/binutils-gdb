f8da11e524a53956d72b430121ff4b3bdbb958be
Andrew Burgess <andrew.burgess@embecosm.com>
2016-03-09 22:08:21 +0000

    patches/00910_1077-rs6000-r29-frame-pointer.patch

--- gdb/rs6000-tdep.c
+++ gdb/rs6000-tdep.c
@@ -1831,6 +1831,15 @@ skip_prologue (struct gdbarch *gdbarch, CORE_ADDR pc, CORE_ADDR lim_pc,
 
 	  /* Another way to set up the frame pointer.  */
 	}
+      else if (op == 0x603d0000	/* oril r29, r1, 0x0 */)
+	{			
+	  fdata->frameless = 0;
+	  framep = 1;
+	  fdata->alloca_reg = (tdep->ppc_gp0_regnum + 29);
+	  continue;
+
+	  /* Another way to set up the frame pointer.  */
+	}
       else if (op == 0x603e0000	/* oril r30, r1, 0x0 */)
 	{			
 	  fdata->frameless = 0;
