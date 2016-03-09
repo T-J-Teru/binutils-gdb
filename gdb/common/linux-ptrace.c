e6add5aa25f70f3c08ae982074160dcc3ad6beed
Andrew Burgess <andrew.burgess@embecosm.com>
2016-03-09 22:08:33 +0000

    patches/02740_34727-fix-nx-test-alarm.patch

--- gdb/common/linux-ptrace.c
+++ gdb/common/linux-ptrace.c
@@ -103,6 +103,7 @@ linux_ptrace_test_ret_to_nx (void)
 		 strerror (errno));
       else
 	{
+	  alarm(2);
 #if defined __i386__
 	  asm volatile ("pushl %0;"
 			".globl linux_ptrace_test_ret_to_nx_instr;"
