8d1dd21a2471ebb726b19a69cb1cd6cbd5d30eca
Andrew Burgess <andrew.burgess@embecosm.com>
2016-05-19 20:47:52 +0100

    FIXES: More fixes for dynamic types.
    This one is taken from upstream.

--- gdb/gdbtypes.c
+++ gdb/gdbtypes.c
@@ -2115,7 +2115,9 @@ resolve_dynamic_struct (struct type *type,
 
       pinfo.type = check_typedef (TYPE_FIELD_TYPE (type, i));
       pinfo.valaddr = addr_stack->valaddr;
-      pinfo.addr = addr_stack->addr;
+      pinfo.addr
+	= (addr_stack->addr
+	   + (TYPE_FIELD_BITPOS (resolved_type, i) / TARGET_CHAR_BIT));
       pinfo.next = addr_stack;
 
       TYPE_FIELD_TYPE (resolved_type, i)
@@ -4672,17 +4674,18 @@ copy_type (const struct type *type)
 {
   struct type *new_type;
 
-  gdb_assert (TYPE_OBJFILE_OWNED (type));
-
   new_type = alloc_type_copy (type);
   TYPE_INSTANCE_FLAGS (new_type) = TYPE_INSTANCE_FLAGS (type);
   TYPE_LENGTH (new_type) = TYPE_LENGTH (type);
   memcpy (TYPE_MAIN_TYPE (new_type), TYPE_MAIN_TYPE (type),
 	  sizeof (struct main_type));
   if (TYPE_DYN_PROP_LIST (type) != NULL)
-    TYPE_DYN_PROP_LIST (new_type)
-      = copy_dynamic_prop_list (&TYPE_OBJFILE (type) -> objfile_obstack,
-				TYPE_DYN_PROP_LIST (type));
+    {
+      gdb_assert (TYPE_OBJFILE_OWNED (type));
+      TYPE_DYN_PROP_LIST (new_type)
+	= copy_dynamic_prop_list (&TYPE_OBJFILE (type) -> objfile_obstack,
+				  TYPE_DYN_PROP_LIST (type));
+    }
 
   return new_type;
 }
