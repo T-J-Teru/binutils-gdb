53c6bb82e1f6afc2e7135fb154a4f510a743c9a5
Andrew Burgess <andrew.burgess@embecosm.com>
2016-05-26 14:00:29 +0100

    gdb: Resolve a couple of the merge conflicts
    Resolve two of the outstanding merge conflicts.  Turns out that the
    uda-plugin.c case wasn't actually a merge conflict at all, I'd just
    added a printf before an abort() that was part of the original code.

--- gdb/aix-thread.c
+++ gdb/aix-thread.c
@@ -809,11 +809,6 @@ sync_threadlists (void)
 
 	  if (cmp_result == 0)
 	    {
-	      /* APB: While merging 215e24af5fc318ad128eef71e there was a
-		 conflict here, not sure if this was resolved correctly
-		 though.  */
-	      fprintf (stderr, "APB: %s:%d\n", __FILE__, __LINE__);
-	      abort ();
 	      if (!gbuf[gi]->priv)
 		gbuf[gi]->priv =
 		  xmalloc (sizeof (struct private_thread_info));
