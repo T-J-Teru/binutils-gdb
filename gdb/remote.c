b3d260f9481f8ac317b99a0d9cf48714a4354496
Andrew Burgess <andrew.burgess@embecosm.com>
2020-11-12 09:20:32 +0000

    [WIP] gdb: better remote packet timeout support
    Now instead of priming the buffer with null contents, place an error
    string into the buffer.  If we get a timeout the packet will now be
    considered an error.  This indicates that the packet is supported, but
    the operation didn't succeed.
    
    I've updated the test so that it spots the error emitted when the
    timeout occurs.
    
    Interestingly with this error in place, after the first iteration of
    the test we now get an internal error (assertion) failure when we
    attempt the second attach:
    
      thread.c:95: internal-error: thread_info* inferior_thread(): Assertion `current_thread_ != nullptr' failed.
    
    So I really need to figure this out too....

--- gdb/remote.c
+++ gdb/remote.c
@@ -9653,12 +9653,8 @@ remote_target::getpkt_or_notif_sane_1 (gdb::char_vector *buf,
      previously cached response.  */
   rs->cached_wait_status = 0;
 
-  /* Set the first character to NULL.  If we get a timeout then when the
-     packet finds its way to packet_ok the packet will be classified as
-     PACKET_UNKNOWN.  Once upon a time we wrote the string "timeout" into
-     the buffer here, this will be classified as PACKET_OK, which means GDB
-     will interpret a timeout while fetching a packet as success.  */
-  *(buf->data ()) ='\0';
+  /* Prime the buffer with a timeout error.  */
+  strcpy (buf->data (), "E.timeout");
 
   if (forever)
     timeout = watchdog > 0 ? watchdog : -1;
