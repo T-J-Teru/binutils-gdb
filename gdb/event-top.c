dc29ff34a274de9b413a86bdf60f3fb737ad8f14
Andrew Burgess <andrew.burgess@embecosm.com>
2021-06-18 12:56:03 +0100

    gdb: rewrite header comment on async_init_signals
    The header comment on the function async_init_signals was getting
    quite out of date, this commit just rewrites to (hopefully) reflect
    the current reality.
    
    There should be no user visible change after this commit.
    
    gdb/ChangeLog:
    
    	* event-top.c (async_init_signals): Rewrite the header comment.

--- gdb/event-top.c
+++ gdb/event-top.c
@@ -899,18 +899,21 @@ handle_sigsegv (int sig)
    handler.  */
 static struct serial_event *quit_serial_event;
 
-/* Initialization of signal handlers and tokens.  There is a function
-   handle_sig* for each of the signals GDB cares about.  Specifically:
-   SIGINT, SIGQUIT, SIGTSTP, SIGHUP, SIGWINCH.  These
-   functions are the actual signal handlers associated to the signals
-   via calls to signal().  The only job for these functions is to
-   enqueue the appropriate event/procedure with the event loop.  Such
-   procedures are the old signal handlers.  The event loop will take
-   care of invoking the queued procedures to perform the usual tasks
-   associated with the reception of the signal.  */
-/* NOTE: 1999-04-30 This is the asynchronous version of init_signals.
-   init_signals will become obsolete as we move to have to event loop
-   as the default for gdb.  */
+/* Initialization of signal handlers and tokens.  There are a number of
+   different strategies for handling different signals here.
+
+   For SIGINT, SIGTERM, SIGQUIT, SIGHUP, SIGTSTP, there is a function
+   handle_sig* for each of these signals.  These functions are the actual
+   signal handlers associated to the signals via calls to signal().  The
+   only job for these functions is to enqueue the appropriate
+   event/procedure with the event loop.  The event loop will take care of
+   invoking the queued procedures to perform the usual tasks associated
+   with the reception of the signal.
+
+   For SIGTRAP we just ensure its disposition is set to SIG_DFL.
+
+   For SIGSEGV the handle_sig* function does all the work for handling this
+   signal.  */
 void
 async_init_signals (void)
 {
