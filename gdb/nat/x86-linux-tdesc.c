dariocastillo11/binutils-gdb-objdump-plugin

gdb/gdbserver: share some code relating to target description creation
This commit is part of a series to share more of the x86 target
description creation code between GDB and gdbserver.

Unlike previous commits which were mostly refactoring, this commit is
the first that makes a real change, though that change should mostly
be for gdbserver; I've largely adopted the "GDB" way of doing things
for gdbserver, and this fixes a real gdbserver bug.

On a x86-64 Linux target, running the test:

  gdb.server/connect-with-no-symbol-file.exp

results in two core files being created.  Both of these core files are
from the inferior process, created after gdbserver has detached.

In this test a gdbserver process is started and then, after gdbserver
has started, but before GDB attaches, we either delete the inferior
executable, or change its permissions so it can't be read.  Only after
doing this do we attempt to connect with GDB.

As GDB connects to gdbserver, gdbserver attempts to figure out the
target description so that it can send the description to GDB, this
involves a call to x86_linux_read_description.

In x86_linux_read_description one of the first things we do is try to
figure out if the process is 32-bit or 64-bit.  To do this we look up
the executable via the thread-id, and then attempt to read the
architecture size from the executable.  This isn't going to work if
the executable has been deleted, or is no longer readable.

And so, as we can't read the executable, we default to an i386 target
and use an i386 target description.

A consequence of using an i386 target description is that addresses
are assumed to be 32-bits.  Here's an example session that shows the
problems this causes.  This is run on an x86-64 machine, and the test
binary (xx.x) is a standard 64-bit x86-64 binary:

  shell_1$ gdbserver --once localhost :54321 /tmp/xx.x

  shell_2$ gdb -q
  (gdb) set sysroot
  (gdb) shell chmod 000 /tmp/xx.x
  (gdb) target remote :54321
  Remote debugging using :5432

gdb/nat/x86-linux-tdesc.c
@@ -1,0 +1,124 @@
+/* Target description related code for GNU/Linux x86 (i386 and x86-64).
+
+   Copyright (C) 2024 Free Software Foundation, Inc.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include "gdbsupport/common-defs.h"
+#include "nat/x86-linux-tdesc.h"
+#ifdef __x86_64__
+#include "arch/amd64.h"
+#endif
+#include "arch/i386.h"
+
+#include "gdbsupport/common-defs.h"
+#include "nat/x86-linux.h"
+#include "nat/x86-linux-dregs.h"
+#include "nat/gdb_ptrace.h"
+#include "nat/x86-xstate.h"
+#include "nat/x86-linux-tdesc.h"
+
+#include <cstdint>
+#include <cstdlib>
+#include <linux/uio.h>
+#include <elf.h>
+#include <sys/user.h>
+#include <sys/user.h>
+
+/* See nat/x86-linux-tdesc.h.  */
+
+const target_desc *
+x86_linux_tdesc_for_tid (int tid, enum tribool *have_ptrace_getregset,
+			 gdb::function_view<void (uint64_t)> xcr0_init_cb,
+			 const char *error_msg, uint64_t *xcr0_storage)
+{
+#ifdef __x86_64__
+
+  x86_linux_arch_size arch_size = x86_linux_ptrace_get_arch_size (tid);
+  bool is_64bit = arch_size.is_64bit ();
+  bool is_x32 = arch_size.is_x32 ();
+
+  if (sizeof (void *) == 4 && is_64bit && !is_x32)
+    error ("%s", error_msg);
+
+#elif HAVE_PTRACE_GETFPXREGS
+  if (have_ptrace_getfpxregs == -1)
+    {
+      elf_fpxregset_t fpxregs;
+
+      if (ptrace (PTRACE_GETFPXREGS, tid, 0, (int) &fpxregs) < 0)
+	{
+	  have_ptrace_getfpxregs = 0;
+	  *have_ptrace_getregset = TRIBOOL_FALSE;
+	  return i386_linux_read_description (X86_XSTATE_X87_MASK);
+	}
+    }
+#endif
+
+  if (*have_ptrace_getregset == TRIBOOL_UNKNOWN)
+    {
+      uint64_t xstateregs[(X86_XSTATE_SSE_SIZE / sizeof (uint64_t))];
+      struct iovec iov;
+
+      iov.iov_base = xstateregs;
+      iov.iov_len = sizeof (xstateregs);
+
+      /* Check if PTRACE_GETREGSET works.  */
+      if (ptrace (PTRACE_GETREGSET, tid,
+		  (unsigned int) NT_X86_XSTATE, &iov) < 0)
+	{
+	  *have_ptrace_getregset = TRIBOOL_FALSE;
+	  *xcr0_storage = 0;
+	}
+      else
+	{
+	  *have_ptrace_getregset = TRIBOOL_TRUE;
+
+	  /* Get XCR0 from XSAVE extended state.  */
+	  *xcr0_storage = xstateregs[(I386_LINUX_XSAVE_XCR0_OFFSET
+				      / sizeof (uint64_t))];
+
+#ifdef __x86_64__
+	  /* No MPX on x32.  */
+	  if (is_64bit && is_x32)
+	    *xcr0_storage &= ~X86_XSTATE_MPX;
+#endif /* __x86_64__ */
+
+	  xcr0_init_cb (*xcr0_storage);
+	}
+    }
+
+  /* Check the native XCR0 only if PTRACE_GETREGSET is available.  If
+     PTRACE_GETREGSET is not available then set xcr0_features_bits to
+     zero so that the "no-features" descriptions are returned by the
+     switches below.  */
+  uint64_t xcr0_features_bits;
+  if (*have_ptrace_getregset == TRIBOOL_TRUE)
+    xcr0_features_bits = *xcr0_storage & X86_XSTATE_ALL_MASK;
+  else
+    xcr0_features_bits = 0;
+
+#ifdef __x86_64__
+  if (is_64bit)
+    {
+      return amd64_linux_read_description (xcr0_features_bits, is_x32);
+    }
+  else
+#endif
+    return i386_linux_read_description (xcr0_features_bits);
+
+  gdb_assert_not_reached ("failed to return tdesc");
+}

gdbserver/linux-amd64-ipa.cc
@@ -23,6 +23,7 @@
 #include "tracepoint.h"
 #include "linux-x86-tdesc.h"
 #include "gdbsupport/x86-xstate.h"
+#include "nat/x86-linux-tdesc.h"
 
 /* Defined in auto-generated file amd64-linux.c.  */
 void init_registers_amd64_linux (void);

gdbserver/linux-i386-ipa.cc
@@ -23,6 +23,7 @@
 #include "tracepoint.h"
 #include "linux-x86-tdesc.h"
 #include "gdbsupport/x86-xstate.h"
+#include "nat/x86-linux-tdesc.h"
 
 /* GDB register numbers.  */
 

gdbserver/linux-x86-tdesc.cc
@@ -26,6 +26,7 @@
 #include "arch/amd64.h"
 #endif
 #include "x86-tdesc.h"
+#include "nat/x86-linux-tdesc.h"
 
 /* Return the right x86_linux_tdesc index for a given XCR0.  Return
    X86_TDESC_LAST if can't find a match.  */

T-J-Teru/binutils-gdb

gdb/nat/x86-linux-tdesc.c
@@ -1,0 +1,124 @@
+/* Target description related code for GNU/Linux x86 (i386 and x86-64).
+
+   Copyright (C) 2024 Free Software Foundation, Inc.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include "gdbsupport/common-defs.h"
+#include "nat/x86-linux-tdesc.h"
+#ifdef __x86_64__
+#include "arch/amd64.h"
+#include "arch/amd64-linux-tdesc.h"
+#endif
+#include "arch/i386.h"
+#include "arch/i386-linux-tdesc.h"
+
+#include "nat/x86-linux.h"
+#include "nat/gdb_ptrace.h"
+#include "nat/x86-xstate.h"
+
+#include <linux/uio.h>
+#include <elf.h>
+#include <sys/user.h>
+
+#ifndef IN_PROCESS_AGENT
+
+/* See nat/x86-linux-tdesc.h.  */
+
+const target_desc *
+x86_linux_tdesc_for_tid (int tid, enum tribool *have_ptrace_getregset,
+			 gdb::function_view<void (uint64_t)> xcr0_init_cb,
+			 const char *error_msg, uint64_t *xcr0_storage)
+{
+#ifdef __x86_64__
+
+  x86_linux_arch_size arch_size = x86_linux_ptrace_get_arch_size (tid);
+  bool is_64bit = arch_size.is_64bit ();
+  bool is_x32 = arch_size.is_x32 ();
+
+  if (sizeof (void *) == 4 && is_64bit && !is_x32)
+    error ("%s", error_msg);
+
+#elif HAVE_PTRACE_GETFPXREGS
+  if (have_ptrace_getfpxregs == -1)
+    {
+      elf_fpxregset_t fpxregs;
+
+      if (ptrace (PTRACE_GETFPXREGS, tid, 0, (int) &fpxregs) < 0)
+	{
+	  have_ptrace_getfpxregs = 0;
+	  *have_ptrace_getregset = TRIBOOL_FALSE;
+	  return i386_linux_read_description (X86_XSTATE_X87_MASK);
+	}
+    }
+#endif
+
+  if (*have_ptrace_getregset == TRIBOOL_UNKNOWN)
+    {
+      uint64_t xstateregs[(X86_XSTATE_SSE_SIZE / sizeof (uint64_t))];
+      struct iovec iov;
+
+      iov.iov_base = xstateregs;
+      iov.iov_len = sizeof (xstateregs);
+
+      /* Check if PTRACE_GETREGSET works.  */
+      if (ptrace (PTRACE_GETREGSET, tid,
+		  (unsigned int) NT_X86_XSTATE, &iov) < 0)
+	{
+	  *have_ptrace_getregset = TRIBOOL_FALSE;
+	  *xcr0_storage = 0;
+	}
+      else
+	{
+	  *have_ptrace_getregset = TRIBOOL_TRUE;
+
+	  /* Get XCR0 from XSAVE extended state.  */
+	  *xcr0_storage = xstateregs[(I386_LINUX_XSAVE_XCR0_OFFSET
+				      / sizeof (uint64_t))];
+
+#ifdef __x86_64__
+	  /* No MPX on x32.  */
+	  if (is_64bit && is_x32)
+	    *xcr0_storage &= ~X86_XSTATE_MPX;
+#endif /* __x86_64__ */
+
+	  xcr0_init_cb (*xcr0_storage);
+	}
+    }
+
+  /* Check the native XCR0 only if PTRACE_GETREGSET is available.  If
+     PTRACE_GETREGSET is not available then set xcr0_features_bits to
+     zero so that the "no-features" descriptions are returned by the
+     switches below.  */
+  uint64_t xcr0_features_bits;
+  if (*have_ptrace_getregset == TRIBOOL_TRUE)
+    xcr0_features_bits = *xcr0_storage & X86_XSTATE_ALL_MASK;
+  else
+    xcr0_features_bits = 0;
+
+#ifdef __x86_64__
+  if (is_64bit)
+    {
+      return amd64_linux_read_description (xcr0_features_bits, is_x32);
+    }
+  else
+#endif
+    return i386_linux_read_description (xcr0_features_bits);
+
+  gdb_assert_not_reached ("failed to return tdesc");
+}
+
+#endif /* !IN_PROCESS_AGENT */

gdbserver/linux-amd64-ipa.cc
@@ -23,6 +23,7 @@
 #include "tracepoint.h"
 #include "linux-x86-tdesc.h"
 #include "gdbsupport/x86-xstate.h"
+#include "arch/amd64-linux-tdesc.h"
 
 /* Defined in auto-generated file amd64-linux.c.  */
 void init_registers_amd64_linux (void);

gdbserver/linux-i386-ipa.cc
@@ -23,6 +23,7 @@
 #include "tracepoint.h"
 #include "linux-x86-tdesc.h"
 #include "gdbsupport/x86-xstate.h"
+#include "arch/i386-linux-tdesc.h"
 
 /* GDB register numbers.  */
 

gdbserver/linux-x86-tdesc.cc
@@ -24,8 +24,10 @@
 #include "gdbsupport/x86-xstate.h"
 #ifdef __x86_64__
 #include "arch/amd64.h"
+#include "arch/amd64-linux-tdesc.h"
 #endif
 #include "x86-tdesc.h"
+#include "arch/i386-linux-tdesc.h"
 
 /* Return the right x86_linux_tdesc index for a given XCR0.  Return
    X86_TDESC_LAST if can't find a match.  */