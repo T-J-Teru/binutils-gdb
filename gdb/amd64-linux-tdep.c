dariocastillo11/binutils-gdb-objdump-plugin

gdb/gdbserver: share some code relating to target description creation
This commit is part of a series to share more of the x86 target
description creation code between GDB and gdbserver.

Unlike previous commits which were mostly refactoring, this commit is
the first that makes a real change, though that change should mostly
be for gdbserver; I've largely adopted the "GDB" way of doing things
for gdbserver, and this fixes a real gdbserver bug.

On a x86-64 Linux target, running the test:

  gdb.server/connect-with-no-symbol-file.exp

results in two core files being created.  Both of these core files are
from the inferior process, created after gdbserver has detached.

In this test a gdbserver process is started and then, after gdbserver
has started, but before GDB attaches, we either delete the inferior
executable, or change its permissions so it can't be read.  Only after
doing this do we attempt to connect with GDB.

As GDB connects to gdbserver, gdbserver attempts to figure out the
target description so that it can send the description to GDB, this
involves a call to x86_linux_read_description.

In x86_linux_read_description one of the first things we do is try to
figure out if the process is 32-bit or 64-bit.  To do this we look up
the executable via the thread-id, and then attempt to read the
architecture size from the executable.  This isn't going to work if
the executable has been deleted, or is no longer readable.

And so, as we can't read the executable, we default to an i386 target
and use an i386 target description.

A consequence of using an i386 target description is that addresses
are assumed to be 32-bits.  Here's an example session that shows the
problems this causes.  This is run on an x86-64 machine, and the test
binary (xx.x) is a standard 64-bit x86-64 binary:

  shell_1$ gdbserver --once localhost :54321 /tmp/xx.x

  shell_2$ gdb -q
  (gdb) set sysroot
  (gdb) shell chmod 000 /tmp/xx.x
  (gdb) target remote :54321
  Remote debugging using :5432

gdb/amd64-linux-tdep.c
@@ -43,6 +43,7 @@
 #include "target-descriptions.h"
 #include "expop.h"
 #include "nat/x86-linux.h"
+#include "nat/x86-linux-tdesc.h"
 
 /* The syscall's XML filename for i386.  */
 #define XML_SYSCALL_FILENAME_AMD64 "syscalls/amd64-linux.xml"

gdb/i386-linux-tdep.c
@@ -41,6 +41,7 @@
 #include "i387-tdep.h"
 #include "gdbsupport/x86-xstate.h"
 #include "nat/x86-linux.h"
+#include "nat/x86-linux-tdesc.h"
 
 /* The syscall's XML filename for i386.  */
 #define XML_SYSCALL_FILENAME_I386 "syscalls/i386-linux.xml"

gdbserver/linux-amd64-ipa.cc
@@ -23,6 +23,7 @@
 #include "tracepoint.h"
 #include "linux-x86-tdesc.h"
 #include "gdbsupport/x86-xstate.h"
+#include "nat/x86-linux-tdesc.h"
 
 /* Defined in auto-generated file amd64-linux.c.  */
 void init_registers_amd64_linux (void);

gdbserver/linux-i386-ipa.cc
@@ -23,6 +23,7 @@
 #include "tracepoint.h"
 #include "linux-x86-tdesc.h"
 #include "gdbsupport/x86-xstate.h"
+#include "nat/x86-linux-tdesc.h"
 
 /* GDB register numbers.  */
 

gdbserver/linux-x86-tdesc.cc
@@ -26,6 +26,7 @@
 #include "arch/amd64.h"
 #endif
 #include "x86-tdesc.h"
+#include "nat/x86-linux-tdesc.h"
 
 /* Return the right x86_linux_tdesc index for a given XCR0.  Return
    X86_TDESC_LAST if can't find a match.  */

T-J-Teru/binutils-gdb

gdb/amd64-linux-tdep.c
@@ -41,6 +41,7 @@
 #include "arch/amd64.h"
 #include "target-descriptions.h"
 #include "expop.h"
+#include "arch/amd64-linux-tdesc.h"
 
 /* The syscall's XML filename for i386.  */
 #define XML_SYSCALL_FILENAME_AMD64 "syscalls/amd64-linux.xml"

gdb/i386-linux-tdep.c
@@ -39,6 +39,7 @@
 
 #include "i387-tdep.h"
 #include "gdbsupport/x86-xstate.h"
+#include "arch/i386-linux-tdesc.h"
 
 /* The syscall's XML filename for i386.  */
 #define XML_SYSCALL_FILENAME_I386 "syscalls/i386-linux.xml"

gdbserver/linux-amd64-ipa.cc
@@ -22,6 +22,7 @@
 #include "tracepoint.h"
 #include "linux-x86-tdesc.h"
 #include "gdbsupport/x86-xstate.h"
+#include "arch/amd64-linux-tdesc.h"
 
 /* fast tracepoints collect registers.  */
 

gdbserver/linux-i386-ipa.cc
@@ -22,6 +22,7 @@
 #include "tracepoint.h"
 #include "linux-x86-tdesc.h"
 #include "gdbsupport/x86-xstate.h"
+#include "arch/i386-linux-tdesc.h"
 
 /* GDB register numbers.  */
 

gdbserver/linux-x86-tdesc.cc
@@ -23,8 +23,10 @@
 #include "gdbsupport/x86-xstate.h"
 #ifdef __x86_64__
 #include "arch/amd64.h"
+#include "arch/amd64-linux-tdesc.h"
 #endif
 #include "x86-tdesc.h"
+#include "arch/i386-linux-tdesc.h"
 
 /* Return the right x86_linux_tdesc index for a given XCR0.  Return
    X86_TDESC_LAST if can't find a match.  */