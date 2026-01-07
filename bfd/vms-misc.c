a6ec95324029b7024d49594303d7b560fcab3907
Keith Seitz <keiths@redhat.com>
2026-01-07 09:36:28 -0800

    gdb-rhbz2424325-c23-more-const-fixes.patch
    ;; Backport Keith Seitz's C23 const-correctness fixes (pending upstream review).
    ;; Mailing list: https://sourceware.org/pipermail/binutils/2026-January/...
    ;; Posted 2026-01-07, not yet approved.
    
    Fix some more C23 const-correctness issues
    
    Fedora Rawhide is failing to build due to new glibc header changes
    enforcing const-correctness in functions like strchr and memchr.
    For example:
    
    ../../opcodes/aarch64-dis.c: In function ‘remove_dot_suffix’:
    ../../opcodes/aarch64-dis.c:4027:7: error: assignment discards ‘const’ qualifier from po
    inter target type [-Werror=discarded-qualifiers]
     4027 |   ptr = strchr (inst->opcode->name, '.');
          |       ^
    cc1: all warnings being treated as errors
    
    This patch addresses all the discovered issues with --enable-targets=all
    and regenerates a few cgen files along the way.

--- bfd/vms-misc.c
+++ bfd/vms-misc.c
@@ -492,14 +492,14 @@ get_vms_time_string (unsigned char *tbuf)
 }
 
 /* Create module name from filename (ie, extract the basename and convert it
-   in upper cases).  Works on both VMS and UNIX pathes.
+   in upper cases).  Works on both VMS and UNIX paths.
    The result has to be free().  */
 
 char *
 vms_get_module_name (const char *filename, bool upcase)
 {
   char *fname, *fptr;
-  const char *fout;
+  const char *fout, *p;
 
   /* Strip VMS path.  */
   fout = strrchr (filename, ']');
@@ -511,9 +511,9 @@ vms_get_module_name (const char *filename, bool upcase)
     fout = filename;
 
   /* Strip UNIX path.  */
-  fptr = strrchr (fout, '/');
-  if (fptr != NULL)
-    fout = fptr + 1;
+  p = strrchr (fout, '/');
+  if (p != NULL)
+    fout = p + 1;
 
   fname = strdup (fout);
 
